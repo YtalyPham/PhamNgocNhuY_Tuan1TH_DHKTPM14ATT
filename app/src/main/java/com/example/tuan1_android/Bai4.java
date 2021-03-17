2d0bd0ada55cd3d9c359c0057ade51931e95fffc
YtalyPham <nhuyphamdev@gmail.com>
2021-03-17 17:21:51 +0700

    first commit

---
+++ app/src/main/java/com/example/tuan1_android/Bai4.java
@@ -0,0 +1,14 @@
+package com.example.tuan1_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bai4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai4);
+    }
+}
